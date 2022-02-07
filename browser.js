c7135b068a3fb6c36da1e5927249e91d3f87b73b
abhikumar98 <abhishekkumar35962@gmail.com>
2022-02-08 02:29:45 +0530

    fix: puppeteer again

--- browser.js
+++ browser.js
@@ -2,7 +2,6 @@ const puppeteer = require("puppeteer");
 
 const scrape = async () => {
   const browser = await puppeteer.launch({
-    headless: false,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
   const page = await browser.newPage();
