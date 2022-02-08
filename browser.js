3c008ea2ec1e1231969132c9ffbd77b757b5d560
abhikumar98 <abhishekkumar35962@gmail.com>
2022-02-08 12:39:03 +0530

    fix: user agent

--- browser.js
+++ browser.js
@@ -12,6 +12,10 @@ const scrape = async () => {
   await page.goto("https://etherscan.io/gastracker", {
     waitUntil: "networkidle0",
   });
+  const userAgent =
+    "Mozilla/5.0 (X11; Linux x86_64)" +
+    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Safari/537.36";
+  await page.setUserAgent(userAgent);
 
   console.log("should've gone to url");
 
